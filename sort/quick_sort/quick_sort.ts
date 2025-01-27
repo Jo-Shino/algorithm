a975013f1bc32d0a451350190a7c739997d75d4e
Jo-Shino <joe000042@gmail.com>
2025-01-27 19:18:19 +0900

    fix/🔨 fix the partition algorithm

--- sort/quick_sort/quick_sort.ts
+++ sort/quick_sort/quick_sort.ts
@@ -5,10 +5,10 @@ function partition(array: number[], low: number, high: number): number {
   for (let j = low; j < high; j++) {
     if (array[j] <= pivot) {
       i += 1;
-      array[i], (array[j] = array[j]), array[i];
+      [array[i], array[j]] = [array[j], array[i]];
     }
   }
-  array[i + 1], (array[high] = array[high]), array[i + 1];
+  [array[i + 1], array[high]] = [array[high], array[i + 1]];
   return i + 1;
 }
 
@@ -24,3 +24,7 @@ function quick_sort(array: number[]): number[] {
   _quick_sort(array, 0, array.length - 1);
   return array;
 }
+
+const numbers = [1, 5, 10, 3, 9];
+console.log("ソート前", numbers);
+console.log("ソート後", quick_sort(numbers));
